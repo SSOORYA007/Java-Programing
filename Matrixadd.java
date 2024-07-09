e500f8ee859aa4c526974e6ee0ffb81f5c732856
SOORYA.S <154416428+SSOORYA007@users.noreply.github.com>
2024-07-09 22:58:51 +0530

    Matrixadd
    12.	Write a program for matrix addition?
    Sample Input:
    Mat1 = 	1  2
    5  3
    Mat2 = 	2   3
                 	4   1
    Sample Output:
    Mat Sum = 3    5
    9     4

---
+++ Matrixadd.java
@@ -0,0 +1,16 @@
+class matadd 
+{
+    public static void main(String[] args) 
+    {
+        int a[][]={{1,2},{3,4}}, b[][]={{1,2},{3,4}},c[][]={{0,0},{0,0}},n=2;
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+              {  
+                  c[i][j] = a[i][j] +b[i][j];
+                  System.out.print(c[i][j] + " ");
+              }
+            System.out.println("");
+        }
+    }
+}
