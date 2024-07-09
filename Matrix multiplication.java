e762c3e95a6b17ec003384e28d88d3b988fa27fc
SOORYA.S <154416428+SSOORYA007@users.noreply.github.com>
2024-07-09 22:56:48 +0530

    Create Matrix multiplication.java
    11.	Write a program for matrix multiplication?
    Sample Input:
    Mat1 = 	1  2
    5  3
    Mat2 = 	2   3
                 	4   1
    Sample Output:
    Mat Sum = 10    5
                        22    18

---
+++ Matrix multiplication.java
@@ -0,0 +1,18 @@
+class matmul 
+{
+    public static void main(String[] args) 
+    {
+        int a[][]={{1,2},{3,4}}, b[][]={{1,2},{3,4}},c[][]={{0,0},{0,0}},n=2;
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+              { for (int k = 0; k < n; k++)
+                 {
+                    c[i][j] += a[i][k] * b[k][j];
+                 }
+         System.out.print(c[i][j] + " ");
+              }
+            System.out.println("");
+        }
+    }
+}
