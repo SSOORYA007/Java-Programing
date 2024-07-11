df6c620b85c1e689cabc5c4eea995f4c5b6b940b
SOORYA.S <154416428+SSOORYA007@users.noreply.github.com>
2024-07-11 23:10:53 +0530

    Fibonacci.java
    Write a program to print the Fibonacci series.
    Sample Input:
    Enter the n value: 6
    Sample Output:
    0	1	1	2	3	5

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,8 @@
 class SI {
     public static void main(String[] args)
-    {   int f1=0,f2=1,f3,n=4;
+    {   int f1=0,f2=1,f3,n=6;
         System.out.print(f1+" "+f2+" ");
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
            f3=f1+f2;
            System.out.print(f3+" ");
