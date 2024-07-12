037542cf3ae06f8cb07acf00c2c7edd8af09d6c4
SOORYA.S <154416428+SSOORYA007@users.noreply.github.com>
2024-07-12 22:36:32 +0530

    Square_and_cube.java
    Write a program to find the square, cube of the given decimal number
    Sample Input:
    Given Number: 0.6
    Sample Output:
    Square Number: 0.36
    Cube Number:0.216

---
+++ Square_and_cube.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double num = 0.6; 
+        double square = num * num;
+        double cube = num * num * num;
+        System.out.println("Square Number: " + square);
+        System.out.println("Cube Number: " + cube);
+    }
+}
